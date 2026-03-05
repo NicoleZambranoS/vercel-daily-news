575af33917fb5ccba14fb487e8c6779a431d5920
Nicole Zambrano <nicole@monogram.io>
2026-03-05 11:41:44 -0500

    Add Footer component and use it into the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header";
 import { geistSans, geistMono } from "@/components/ui/font";
+import { Footer } from "@/components/footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,6 +21,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
